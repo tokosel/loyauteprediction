0d126fbff9f38b67d25e78b0494f739b07c25782
tokosel <abdoulayesallboke@outlook.fr>
2024-01-12 15:13:45 +0000

    Test de normalité

--- model.R
+++ model.R
@@ -1,9 +1,13 @@
 # Import des librairies utiles
 #install.packages("shiny")
+#install.packages("nortest")
+
+
 library(dplyr)
 library(ggplot2)
 library(caret)
 library(MASS)
+library(nortest)
 library(shiny)
 
 # Import des données
@@ -16,6 +20,16 @@ summary(df)
 # Vérification des valeurs manquantes 
 sum(is.na(df))
 # On voit qu'il n'y a pas de VM
+
+# Test de normalité
+
+# Test de Shapiro wilk
+shapiro.test(df$Montant.Depense..Franc.CFA.)
+shapiro.test(df$Nombre.de.Produits.Achetes)
+
+# Test de Kolmogorov-Smirnov
+ks.test(df$Montant.Depense..Franc.CFA.,"pnorm", mean(df$Montant.Depense..Franc.CFA.), sd(df$Montant.Depense..Franc.CFA.))
+ks.test(df$Nombre.de.Produits.Achetes,"pnorm", mean(df$Montant.Depense..Franc.CFA.), sd(df$Montant.Depense..Franc.CFA.)) 
 
 # Préparation des données 
 df$Drug = factor(df$Drug)
