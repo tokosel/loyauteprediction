0c9ad467d41a19751c5a332e90b8d53ad13264e6
tokosel <abdoulayesallboke@outlook.fr>
2024-01-17 13:39:56 +0000

    Modelisation

--- model.R
+++ model.R
@@ -1,10 +1,9 @@
 # Import des librairies utiles
-#install.packages("shiny")
 library(dplyr)
 library(ggplot2)
 library(caret)
 library(MASS)
-library(shiny)
+#library(shiny)
 
 # Import des données
 df = read.csv("sendawal.csv")
@@ -30,12 +29,8 @@ barplot(table(df$Loyaute), main = "Fréquence des Niveaux de Loyaute", xlab = "L
 # Relation entre Montant.Depense..Franc.CFA. et Nombre.de.Produits.Achetes
 plot(df$Montant.Depense..Franc.CFA., df$Nombre.de.Produits.Achetes, main = "Relation entre Montant Total Dépensé et Nombre de Produits Achetés", xlab = "Montant Total Dépensé (Franc CFA)", ylab = "Nombre de Produits Achetés")
 
-# Utilisez la fonction pairs() pour examiner les relations entre toutes les paires de variables numériques
-pairs(df[, c("Nombre.de.Produits.Achetes", "Montant.Depense..Franc.CFA.")])
-
 # Vérification des valeurs manquantes 
-sum(is.na(df))
-# On voit qu'il n'y a pas de VM
+sum(is.na(df))# On voit qu'il n'y a pas de VM
 
 # Tests de normalité
 
@@ -48,4 +43,39 @@ ks.test(df$Montant.Depense..Franc.CFA.,"pnorm", mean(df$Montant.Depense..Franc.C
 ks.test(df$Nombre.de.Produits.Achetes,"pnorm", mean(df$Montant.Depense..Franc.CFA.), sd(df$Montant.Depense..Franc.CFA.)) 
 
 
-# Analyse Discriminante Quadratique (LDA)
+# Analyse Discriminante Quadratique
+
+# Sélection des variables explicatives et de la variable à prédire 
+df_model <- df[, c("Loyaute","Montant.Depense..Franc.CFA.","Nombre.de.Produits.Achetes")]
+# Diviser les données en ensemble d'apprentissage et de test
+set.seed(123)
+index <- sample(1:nrow(df), 0.7 * nrow(df))
+train <- df_model[index, ]
+train
+test <- df[-index, ]
+test
+# Modélisation
+
+# Créer un modèle LDA
+model <- lda(Loyaute ~ ., data = train)
+model
+summary(model)
+
+# Évaluation du modèle
+
+# Prédire sur l'ensemble de test
+prediction <- predict(model, newdata = test)
+prediction
+# Probabilités d'appartenance à chaque classe pour les premières observations
+head(predict(model, newdata = test, type = "response")$posterior)
+
+# Matrice de confusion
+Matrice <- table(Loyaute = test$Loyaute, Predite = prediction$class)
+Matrice
+
+# Taux de classification
+accuracy <- sum(diag(Matrice)) / sum(Matrice)
+accuracy
+
+
+
