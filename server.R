899ff3f9e5c89485af416847b1c42fa85d08cd4f
tokosel <abdoulayesallboke@outlook.fr>
2024-01-17 14:48:22 +0000

    Review code

--- server.R
+++ server.R
@@ -4,22 +4,36 @@ library(MASS)
 # Charger le modèle LDA
 model <- readRDS("modele_lda.rds")
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
+  observe({
+    # Envoyer les placeholders aux champs numériques
+    session$sendCustomMessage(type = "placeholder", list(id = "produits", value = "Entrez le nombre de produits"))
+    session$sendCustomMessage(type = "placeholder", list(id = "montant", value = "Entrez le montant dépensé"))
+  })
   
   observeEvent(input$predictBtn, {
     # Récupérer les valeurs entrées par l'utilisateur
     nombre_produits <- input$produits
     montant_depense <- input$montant
     
-    # Effectuer la prédiction avec le modèle LDA
-    new_data <- data.frame(Nombre.de.Produits.Achetes = nombre_produits,
-                           Montant.Depense..Franc.CFA. = montant_depense)
-    
-    prediction <- predict(model, newdata = new_data)
-    
-    # Afficher la prédiction
-    output$predictionText <- renderText({
-      paste("La loyauté prédite du client est :", prediction$class)
-    })
+    # Vérifier les contraintes
+    if (is.null(nombre_produits) || is.null(montant_depense) || nombre_produits <= 0 || montant_depense <= 0) {
+      # Afficher un message d'erreur temporaire
+      showNotification("Le nombre de produits et le montant dépensé doivent être supérieurs à zéro.", type = "error", duration = 10)
+      
+    } else {
+      
+      # Effectuer la prédiction avec le modèle LDA
+      new_data <- data.frame(Nombre.de.Produits.Achetes = nombre_produits,
+                             Montant.Depense..Franc.CFA. = montant_depense)
+      
+      prediction <- predict(model, newdata = new_data)
+      
+      # Afficher la prédiction
+      output$predictionText <- renderText({
+        paste("La loyauté prédite de ce client est :", prediction$class)
+      })
+    }
   })
 })
--- ui.R
+++ ui.R
@@ -1,16 +1,18 @@
+# ui.R
+
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Prédiction de la Loyauté du Client"),
+  titlePanel(div("Prédiction de la loyauté des clients de Sendawal", style = "color: #6fbc30;")),
   
   sidebarLayout(
     sidebarPanel(
       numericInput("produits", "Nombre de Produits Achetés :", value = 1, min = 1),
-      numericInput("montant", "Montant Dépensé (Franc CFA) :", value = 100, min = 0),
-      actionButton("predictBtn", "Prédire la Loyauté")
+      numericInput("montant", "Montant Dépensé (Franc CFA) :", value = 100, min = 100),
+      actionButton("predictBtn", "Prédire la Loyauté", style = "color: #ffffff; background-color: #6fbc30; border-color: #6fbc30;")
     ),
     mainPanel(
-      h3("Résultat de la Prédiction :"),
+      h4("Résultat de la Prédiction :", style = "color: #6fbc30;"),
       verbatimTextOutput("predictionText")
     )
   )
